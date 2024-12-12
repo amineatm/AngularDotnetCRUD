1249b28cc7f46d42e0e11b9085ff5b38d5be8959
mohamedamine.atmani@ax-lab.com <mohamedamine.atmani@ax-lab.com>
2024-12-13 00:39:18 +0100

    Add AuthECClientApi and AuthECClientApp

--- AuthECClientApi/AuthECClientApi/obj/Debug/net8.0/AuthECClientApi.AssemblyInfo.cs
+++ AuthECClientApi/AuthECClientApi/obj/Debug/net8.0/AuthECClientApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthECClientApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9800c78d11e752bd9d005f29babcfacdd2893025")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthECClientApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthECClientApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
